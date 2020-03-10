57bad8b1eb045a4481e4667fac3fb89423d2259b
Mihir Gadgil <16473290+scimas@users.noreply.github.com>
2020-03-10 15:56:10 -0400

    Stop using var

--- js/index.js
+++ js/index.js
@@ -4,16 +4,16 @@ d3.csv('./data/olympics_data.csv', function(d) {
         Medal: d.Medal
     };
 }).then(function(data) {
-    var gold = d3.sum(data, (d) => (d.Medal == "Gold"));
-    var silver = d3.sum(data, (d) => (d.Medal == "Silver"));
-    var bronze = d3.sum(data, (d) => (d.Medal == "Bronze"));
+    let gold = d3.sum(data, (d) => (d.Medal == "Gold"));
+    let silver = d3.sum(data, (d) => (d.Medal == "Silver"));
+    let bronze = d3.sum(data, (d) => (d.Medal == "Bronze"));
     
-    var gold_caption = d3.select('#gold-medal').select('figcaption');
+    let gold_caption = d3.select('#gold-medal').select('figcaption');
     gold_caption.text(gold);
 
-    var silver_caption = d3.select('#silver-medal').select('figcaption');
+    let silver_caption = d3.select('#silver-medal').select('figcaption');
     silver_caption.text(silver);
 
-    var bronze_caption = d3.select('#bronze-medal').select('figcaption');
+    let bronze_caption = d3.select('#bronze-medal').select('figcaption');
     bronze_caption.text(bronze);
 });
